18f9456f9849dfd946154d11c461da69d9239020
Aniket Kumar <aniketkumar049@gmail.com>
2021-02-25 11:10:00 +0530

    sample code added

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Text} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import Layout from '../Layout/MainLayout/MainLayout';
 const Home = () => {
   return (
     <Layout>
       <Text>Hello</Text>
+      <Icon name="facebook" color="#3b5998" />
     </Layout>
   );
 };
